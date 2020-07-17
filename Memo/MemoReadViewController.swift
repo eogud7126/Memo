d841cd5781148ad8577a27304218e0cd30f93eca
DaeHyung <eogudlee@worksmobile.com>
2020-07-17 15:30:18 +0900

    테이블 뷰 클릭시 상세화면 전환 및 데이터 배치.

--- Memo/MemoReadViewController.swift
+++ Memo/MemoReadViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class MemoReadViewController: UIViewController {
 
+    //MARK: - Properties
+    
+    //콘텐츠를 저장하는 변수
+    var param: MemoData?
+    
+    @IBOutlet var subject: UILabel!
+    @IBOutlet var contents: UILabel!
+    @IBOutlet var img: UIImageView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
 
+        //제목, 내용, 이미지 출력
+        self.subject.text = param?.title
+        self.contents.text = param?.contents
+        self.img.image = param?.image
+        
+        //날짜 포맷 변환
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd일 HH:mm분에 작성됨"
+        let dateString = formatter.string(from: (param?.regdate)!)
+        
+        //네비게이션 타이틀에 날짜 표시
+        self.navigationItem.title = dateString
         // Do any additional setup after loading the view.
     }
     
--- Memo/MemoTableViewController.swift
+++ Memo/MemoTableViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MemoTableViewController: UITableViewController {
 
+    //앱 델리게이트 참조 정보 가져옴
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,29 +23,67 @@ class MemoTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    //뷰가 화면에 출력되면 호출
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //테이블 데이터 리로드
+        self.tableView.reloadData()
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
+    //셀의 개수 결정
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.appDelegate.memolist.count
     }
 
-    /*
+    //개별 행을 구성하는 메소드
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        //memolist 배열에서 주어진 행에 맞는 데이터 꺼냄
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        //*******이미지 속성이 비어있고 없고에 따라 프로토타입 셀 식별자를 변경*******
+        let cellId = row.image == nil ? "memoCell" : "memoCellwithImage"
+        
+        //재사용 큐로부터 프로토타입 셀의 인스턴스를 전달 받음
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! MemoTableViewCell
+        
+        //내용 구성
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        //Date타입의 날짜를 포맷에 맞게 변경
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text = formatter.string(from: row.regdate!)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //memolist에서 선택된 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        //상세화면 인스턴스 생성
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemoRead") as? MemoReadViewController else{
+            return
+        }
+        
+        //값을 전달한 다음 상세 화면으로 이동
+        vc.param = row
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
